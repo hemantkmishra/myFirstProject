a35f0d0d0bf601d2a42e117bcd3e9a4fd19c0c02
hemant mishra <hemantcs35@gmail.com.com>
2018-10-14 16:52:17 +0530

    added dashboard, login page etc.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.post('/loginform', function(req, res, next) {
+	let data = req.body
+	console.log(data)
+  res.render('dashboard', { title: 'dashboard', name : data.name });
+});
+
 module.exports = router;
